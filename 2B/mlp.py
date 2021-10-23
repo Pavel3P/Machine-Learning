b7e08fe75ef1650fcca45ef1893d9ca5abe4ebea
Pavel Pelikh <barashkaaaa@gmail.com>
2021-10-24 01:47:16 +0300

    MLP class refactoring
    Added threshold parameter for prediction
    function.
    
    Added shuffling of samples on each epoch
    in train function.
    
    Added description, some code cleaning.

--- 2B/mlp.py
+++ 2B/mlp.py
@@ -4,31 +4,39 @@ from misc.activation_functions import \
 
 
 class MLP:
+    """
+    Implementation of Multilayer Perceptron
+    with backpropagation training function
+    """
+
     activation_functions: dict[str: tuple[callable, callable]] = {
         "sigmoid": (np.vectorize(sigmoid), np.vectorize(sigmoid_derivative)),
     }
 
     def __init__(self,
                  size: list[int],
+                 threshold: float = .5,
                  activation: str = "sigmoid",
-                 l_rate: float = 0,
+                 l_rate: float = 1,
                  ) -> None:
         """
 
-        :param size: number of neurons in each layer
-                          including input layer
-        :param activation:
+        :param size: number of neurons in each layer including input layer
+        :param threshold: parameter for thresholding activation function
+                          in prediction function
+        :param activation: type of activation function
         :param l_rate: learning rate
         """
         self.size: list[int] = size
         self.weights: list[np.ndarray] = []
+        self.threshold: float = threshold
         self.l_rate: float = l_rate
         self.activation, self.act_der = self.activation_functions[activation]
 
     def _forward(self,
-                 X: np.ndarray
+                 x: np.ndarray
                  ) -> list[np.ndarray]:
-        outputs: list[np.ndarray] = [X]
+        outputs: list[np.ndarray] = [np.array([x]).T]
         for weight_matrix in self.weights:
             output: np.ndarray = self.activation(weight_matrix @ outputs[-1])
             outputs.append(output)
@@ -43,16 +51,20 @@ class MLP:
         new_weights: list[np.ndarray] = []
 
         for i in range(len(self.weights))[::-1]:
-            new_weight_matrix = self.weights[i] - self.l_rate * np.tensordot(errors, outputs[i-1])
+            new_weight_matrix = self.weights[i] - self.l_rate * np.tensordot(
+                                                                        errors.reshape(errors.shape[0]),
+                                                                        outputs[i].reshape(outputs[i].shape[0]),
+                                                                        axes=0
+                                                                            )
             new_weights.insert(0, new_weight_matrix)
             errors = self.weights[i].T @ errors
 
-        return new_weights[::-1]
+        return new_weights
 
     def _random_init(self
                      ) -> list[np.ndarray]:
         weights: list[np.ndarray] = []
-        for rows, cols in zip(self.size, self.size[1:]):
+        for cols, rows in zip(self.size, self.size[1:]):
             weight_matrix = np.random.uniform(-.5, .5, (rows, cols))
             weights.append(weight_matrix)
 
@@ -66,11 +78,14 @@ class MLP:
         self.weights: list[np.ndarray] = self._random_init()
 
         for epoch in range(epochs):
-            for x, y in zip(X, Y):
+            idxs: np.ndarray = np.arange(len(X))
+            np.random.shuffle(idxs)
+
+            for x, y in zip(X[idxs], Y[idxs]):
                 outputs = self._forward(x)
                 self.weights = self._back(outputs, y)
 
     def predict(self,
-                X: np.ndarray
+                x: np.ndarray
                 ) -> np.ndarray:
-        return self._forward(X)[-1]
+        return (self._forward(x)[-1] > self.threshold).astype(int)
