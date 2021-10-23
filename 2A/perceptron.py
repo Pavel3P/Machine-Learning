e8fe62af8384fed591a955401b04d1c7651998e7
Pavel Pelikh <barashkaaaa@gmail.com>
2021-10-23 15:04:40 +0300

    Perceptron class finishing
    Implemented train function,
    prediction function.
    
    Added description, cleaned code.

--- 2A/perceptron.py
+++ 2A/perceptron.py
@@ -2,20 +2,34 @@ import numpy as np
 
 
 class Perceptron:
-    def __init__(
-            self,
-    ) -> None:
+    """
+    Implementation of perceptron with
+    Rosenblatt's training algorithm
+    for binary classification.
+    """
+
+    def __init__(self
+                 ) -> None:
         self.w: np.ndarray = None
+        self.b: float = 0
 
-    def train(
-            self,
-            X: np.ndarray,
-            Y: np.ndarray
-    ) -> None:
-        pass
+    def train(self,
+              X: np.ndarray,
+              Y: np.ndarray
+              ) -> None:
+        self.w = np.random.rand(X.shape[1])
+        for x, y in zip(X, Y):
+            error = y - self.predict(x)
+            if error != 0:
+                self.w = self.w + error * x
+                self.b += error
 
-    def predict(
-            self,
-            X: np.ndarray
-    ) -> np.ndarray:
-        pass
+    def predict(self,
+                X: np.ndarray
+                ) -> np.ndarray:
+        return np.heaviside(self._h(X), 0).astype(int)
+
+    def _h(self,
+           X: np.ndarray
+           ) -> np.ndarray:
+        return X @ self.w + self.b
