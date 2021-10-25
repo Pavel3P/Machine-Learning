b9c3863915a03dda9efbdce38ed5856e51629ac7
Pavel Pelikh <barashkaaaa@gmail.com>
2021-10-26 00:23:44 +0300

    Add max iterations parameter

--- 5/kmeans.py
+++ 5/kmeans.py
@@ -5,18 +5,26 @@ class KMeans:
     clusters: np.ndarray
 
     def __init__(self,
-                 k: int
+                 k: int,
+                 max_iter: int
                  ) -> None:
         self.k: int = k
+        self.max_iter: int = max_iter
 
     def train(self,
               X: np.ndarray
               ) -> None:
+
         # Random initialization
-        self.clusters = X[np.random.randint(0, len(X), self.k)]
+        self.clusters = X[np.random.randint(0, len(X), size=self.k)]
+
         predictions: np.ndarray = self.predict(X)
 
-        while True:
+        # Loop until predictions on next iteration
+        # will not change
+        iteration: int = 0
+        while True or iteration < self.max_iter:
+            iteration += 1
             for cluster in range(self.k):
                 cluster_elements: np.ndarray = X[predictions == cluster]
                 self.clusters[cluster, :] = np.mean(cluster_elements, axis=0)
